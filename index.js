2c25bbe07758cb501a39515710079461cdee1233
Mohammad Mohsin <mohammadmohsin28012005@gmail.com>
2025-09-07 23:48:20 +0600

    make a single route and add different type of http operations

--- index.js
+++ index.js
@@ -20,12 +20,22 @@ app.get("/api/users", (req, res) => {
   return res.json(users);
 });
 
-app.get("/users/:id", (req, res) => {
-  const id = Number(req.params.id);
+app
+  .route("/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
 
-  const user = users.find((user) => user.id === id);
-  return res.json(user);
-});
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    // Edit user with Id
+    return res.json({ status: "pending" });
+  })
+  .delete((req, res) => {
+    // Delete user with Id
+    return res.json({ status: "pending" });
+  });
 
 app.listen(PORT, () => {
   console.log(`Server starte at PORT ${PORT}`);
